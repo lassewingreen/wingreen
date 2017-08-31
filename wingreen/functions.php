154a256daa38c9a3c6168bdc78cd68c99ae14309
Lasse Wingreen <lasse@wingreen.dk>
2017-08-31 14:49:15 +0200

    Css play
    Har rodet med menu.. er ikke kommet langt endnu.

--- wingreen/functions.php
+++ wingreen/functions.php
@@ -1,6 +1,16 @@
 <?php 
 
 
+function enqueue_my_scripts() {
+	wp_enqueue_script( 'jquery', '//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js', array('jquery'), '1.9.1', true); // we need the jquery library for Bootstrap js to function
+	wp_enqueue_script( 'bootstrap-js', '//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js', array('jquery'), true); // all the bootstrap JavaScript goodness
+	}
+	add_action('wp_enqueue_scripts', 'enqueue_my_scripts');
+	function enqueue_my_styles() {
+	wp_enqueue_style( 'bootstrap', '//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css' );
+	wp_enqueue_style( 'my-style', get_template_directory_uri() . '/style.css');
+	}
+
 	// Add theme support for Custom Logo.
 	add_theme_support( 'custom-logo', array(
 		'width'       =>"",
@@ -27,17 +37,9 @@ add_filter('upload_mimes', 'cc_mime_types');
 
 
 
-function enqueue_my_scripts() {
-wp_enqueue_script( 'jquery', '//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js', array('jquery'), '1.9.1', true); // we need the jquery library for Bootstrap js to function
-wp_enqueue_script( 'bootstrap-js', '//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js', array('jquery'), true); // all the bootstrap JavaScript goodness
-}
-add_action('wp_enqueue_scripts', 'enqueue_my_scripts');
-function enqueue_my_styles() {
-wp_enqueue_style( 'bootstrap', '//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css' );
-wp_enqueue_style( 'my-style', get_template_directory_uri() . '/style.css');
-}
 
-add_action('wp_enqueue_scripts', 'enqueue_my_styles');
+
+// add_action('wp_enqueue_scripts', 'enqueue_my_styles');
 
 
 
