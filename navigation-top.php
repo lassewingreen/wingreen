9d534e3bfc66e18f5539bf6dc3d1eb5ef0af7d3e
Lasse Wingreen <lasse@wingreen.dk>
2017-09-04 10:21:14 +0200

    CSS
    More fooling around with menu..

---
+++ navigation-top.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Displays top navigation
+ *
+ * @package WordPress
+ * @subpackage Twenty_Seventeen
+ * @since 1.0
+ * @version 1.2
+ */
+
+?>
+<nav id="site-navigation" class="main-navigation" role="navigation" aria-label="<?php esc_attr_e( 'Top Menu', 'twentyseventeen' ); ?>">
+	<button class="menu-toggle" aria-controls="top-menu" aria-expanded="false">
+    <input class="burger-toggle" id="burger" type="checkbox" />
+        
+        <label for"burger"" class="burger-toggle" id="burger_bars" for="burger">
+            <span id="bar1"></span>
+            <span id="bar2"></span>
+            <span id="bar3"></span>
+        </label>
+        <?php
+        //echo twentyseventeen_get_svg( array( 'icon' => 'bars' ) );
+		//echo twentyseventeen_get_svg( array( 'icon' => 'close' ) );
+		//_e( 'Menu', 'twentyseventeen' );
+		?>
+	</button>
+
+	<?php wp_nav_menu( array(
+		'theme_location' => 'top',
+		'menu_id'        => 'top-menu',
+	) ); ?>
+
+	<?php if ( ( twentyseventeen_is_frontpage() || ( is_home() && is_front_page() ) ) && has_custom_header() ) : ?>
+		<a href="#content" class="menu-scroll-down"><?php echo twentyseventeen_get_svg( array( 'icon' => 'arrow-right' ) ); ?><span class="screen-reader-text"><?php _e( 'Scroll down to content', 'twentyseventeen' ); ?></span></a>
+	<?php endif; ?>
+</nav><!-- #site-navigation -->
